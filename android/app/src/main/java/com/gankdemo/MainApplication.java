870d99ad559268149808a75c624404580a206e8e
guuguo <guuguo@qq.com>
2017-10-15 15:50:54 +0800

    新增material kit

--- android/app/src/main/java/com/gankdemo/MainApplication.java
+++ android/app/src/main/java/com/gankdemo/MainApplication.java
@@ -27,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
             new ReactMaterialKitPackage()
       );
     }
-
     @Override
     protected String getJSMainModuleName() {
       return "index";
